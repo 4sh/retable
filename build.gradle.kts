a60c4a2d1cfa412ae4e6171869070d7b6e046d61
a-sarrabayrouse <alexis.sarrabayrouse@4sh.fr>
2022-12-27 11:24:19 +0100

    bump gradle and strikt version + replace deprecated methods

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 object Versions {
     val junit = "5.2.0"
-    val strikt = "0.16.0"
+    val strikt = "0.34.0"
     val commonscsv = "1.9.0"
     val poi = "5.2.3"
 }
-
 
 plugins {
     kotlin("jvm") version "1.6.21"
@@ -12,25 +11,24 @@ plugins {
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
-tasks.getByPath("test").doFirst({
+tasks.getByPath("test").doFirst {
     with(this as Test) {
         useJUnitPlatform()
     }
-})
+}
 
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
-    compile(kotlin("reflect"))
-    compile("org.apache.commons:commons-csv:${Versions.commonscsv}")
-    compile("org.apache.poi:poi:${Versions.poi}")
-    compile("org.apache.poi:poi-ooxml:${Versions.poi}")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("org.apache.commons:commons-csv:${Versions.commonscsv}")
+    implementation("org.apache.poi:poi:${Versions.poi}")
+    implementation("org.apache.poi:poi-ooxml:${Versions.poi}")
 
-    testCompile("io.strikt:strikt-core:${Versions.strikt}")
+    testImplementation("io.strikt:strikt-core:${Versions.strikt}")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.junit}")
     testImplementation("org.junit.jupiter:junit-jupiter-params:${Versions.junit}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.junit}")
 }
-
