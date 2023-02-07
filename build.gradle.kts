49575d8cf44e66a31434d989710a33cc618d8998
a-sarrabayrouse <alexis.sarrabayrouse@4sh.fr>
2023-02-07 14:32:23 +0100

    add OSSRH repo and OSSRH's requirement to publish on maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,18 @@ object Versions {
 plugins {
     kotlin("jvm") version "1.6.21"
     id("maven")
+    id("maven-publish")
+    signing
+    id("com.palantir.git-version") version "0.12.3"
 }
+
+apply<com.palantir.gradle.gitversion.GitVersionPlugin>()
+
+val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+val lastTag: String = versionDetails().lastTag
+
+version = if (lastTag != "") lastTag else "SNAPSHOT"
+group = "io.github.4sh.retable"
 
 repositories {
     mavenCentral()
@@ -17,6 +28,79 @@ repositories {
 tasks.getByPath("test").doFirst {
     with(this as Test) {
         useJUnitPlatform()
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("ExposedJar") {
+            artifactId = "${rootProject.name}-${project.name}"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Retable")
+                description.set("Kotlin library to work with tabular data files")
+                url.set("https://github.com/4sh/retable")
+
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("4SH")
+                        name.set("4SH")
+                        organization.set("4SH")
+                        organizationUrl.set("https://www.4sh.fr")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/4sh/retable")
+                    connection.set("scm:git:git://github.com/4sh/retable.git")
+                    developerConnection.set("scm:git:git@github.com:4sh/retable.git")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val secretKey = System.getenv("SIGNING_KEY")
+    val passphrase = System.getenv("SIGNING_KEY_PASSPHRASE")
+    @Suppress("UnstableApiUsage")
+    useInMemoryPgpKeys(secretKey, passphrase)
+    sign(publishing.publications["ExposedJar"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
     }
 }
 
